80e4749388f6fd258ed9e873bc3875f848427be6
jhon <yc24.95@gmail.com>
2016-05-24 22:01:42 -0500

    primer commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+
+var buildPath = './build';
+
+gulp.task('server', function(){
+	connect.server({
+		root: buildPath,
+		port: 8080,
+		livereload: true
+	});
+});
+
+gulp.task('build:html', function() {
+	gulp.src('./src/**/*.html')
+		.pipe(gulp.dest(buildPath))
+		.pipe(connect.reload());
+});
+
+gulp.task('build:js', function() {
+	gulp.src('./src/js/**/*.js')
+		.pipe(gulp.dest(buildPath+'/js'))
+		.pipe(connect.reload());
+});
+
+gulp.task('build:css', function() {
+	gulp.src('./src/style/main.styl')
+		.pipe(stylus({
+			use: nib()
+		}))
+		.pipe(gulp.dest(buildPath+'/css'))
+		.pipe(connect.reload());
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch('./src/**/*.html', ['build:html']);
+	gulp.watch('./src/**/*.styl', ['build:css']);
+	gulp.watch('./src/**/*.js', ['build:js']);
+});
+
+gulp.task('build', ['build:css', 'build:html', 'build:js']);
+
+gulp.task('default', ['server', 'build', 'watch']);
